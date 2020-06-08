e367661e6d902ecaf6d70694c82be143391c307c
iamcristos <nmereginivincent@gmail.com>
2020-06-08 23:15:41 +0100

    added auth route

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,8 +3,19 @@ class AuthenticationController < ApplicationController
 
     def customer_login
         @customer = Customer.find_by_email(params[:email])
+        
         if @customer&.authenticate(params[:password])
             token = JsonWebToken.encode(user_id: @customer.id)
+            time = Time.now + 24.hours.to_i
+            render json: {token, exp: time.striftime("%m-%d-%Y %H:%M"), email: @customer.email}, status: :ok
+        else
+            render json: {error: 'unathorized'}, status: :unathorized
+        end
+
+        private
+
+        def login_params
+            params.permit(:email, :password)
         end
     end
 end
