b84e78b99ee718d743cbcbae4a0053dd85fbda6d
iamcristos <nmereginivincent@gmail.com>
2020-06-08 22:37:53 +0100

    added more logic

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,15 @@
 class Customer
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  has_secure_password
+
   validates :password, length: { minimum: 8, maximum: 16 }
   validates :password, confirmation: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :email, presence: true, uniqueness: true
 
-  
+
   field :email, type: String
   field :password, type: String
   field :description, type: String 
