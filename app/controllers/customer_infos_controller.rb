a829a86d807f03bd5b6ca9d3be6acc340c202d60
iamcristos <nmereginivincent@gmail.com>
2020-06-18 11:36:41 +0100

    added customer info controller

--- app/controllers/customer_infos_controller.rb
+++ app/controllers/customer_infos_controller.rb
@@ -1,2 +1,18 @@
 class CustomerInfosController < ApplicationController
+
+    def create
+        @customer_info = CustomerInfo.new(customer_info_params)
+
+        if @customer_info.save 
+            render json: @customer_info, status: :created
+        else
+            render json: @customer_info.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def customer_info_params
+        params.permit(:email, :customer_name)
+    end
 end
