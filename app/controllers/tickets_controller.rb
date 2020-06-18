e8a2b387d2dab2a9078fc9546baaaa5a13770db0
iamcristos <nmereginivincent@gmail.com>
2020-06-18 13:19:06 +0100

    completed ticket endpoint

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,24 @@
 class TicketsController < ApplicationController
+
+    def index
+        @tickets = Ticket.all
+
+        render json: @tickets, status: :ok
+    end
+
+    def create
+        @ticket = Ticket.new
+        if @ticket.save 
+            render json: @ticket, status: :created
+        else
+            render json: @ticket.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        
+        if @ticket.update_attributes(:closed => true)
+            render json: @ticket, status: :ok, :message => 'ticket closed succesfully'
+    end
+    
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  field :closed, type: Boolean, default: false
 end
