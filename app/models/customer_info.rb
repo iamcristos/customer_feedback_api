54c65e1749b3d5a6c189201891a953a3d508e6c1
iamcristos <nmereginivincent@gmail.com>
2020-06-18 11:24:39 +0100

    completed customer info model

--- app/models/customer_info.rb
+++ app/models/customer_info.rb
@@ -1,4 +1,10 @@
 class CustomerInfo
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  field :email, type: String
+  field :customer_name, type: String
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :email, presence: true, uniqueness: true
+  validates :customer_name, presence: true
 end
